21885e7231207eaec4e1857bf8b2de4f83cce879
pawelwasik2 <pawelwasik18@gmail.com>
2019-12-03 19:23:44 +0100

    all operations in scala are methods calls

--- src/main/scala/Training.scala
+++ src/main/scala/Training.scala
@@ -1,8 +1,11 @@
 object Training extends App{
+  //instead of for in for loops
+  //(1 to 3).foreach(i => (1 to 3).foreach(j => println(i, j)))
 
-  val u = (1 to 3).foreach(i => (1 to 3).foreach(j=> println(i, j)))
-  println(u)
-
-
+  //1 + 2 invoke method named +, originally its (1).+(2)
+  //the same as in for loop syntax like: for(i <- 1 to 2) equal to for(i <- (1).to(2))
+  //summary: if a method takes only one parameter you can call it without dot or parentheses
+  //println(1 + 2)
+  //println((1).+(2))
 }
 
