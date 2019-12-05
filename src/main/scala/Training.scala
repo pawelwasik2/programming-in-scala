f9794a506392b0d51b2f19ad4c29c5559ddcd97a
pawelwasik2 <pawelwasik18@gmail.com>
2019-12-05 23:02:38 +0100

    lists, tuples, sets, maps, hashsets

--- src/main/scala/Training.scala
+++ src/main/scala/Training.scala
@@ -1,6 +1,6 @@
 object Training extends App{
   //instead of for in for loops
-  //(1 to 3).foreach(i => (1 to 3).foreach(j => println(i, j)))
+  //(1 to 3).foreach(i => (1 to 3).foreach(j => println( i, j)))
 
   //1 + 2 invoke method named +, originally its (1).+(2)
   //the same as in for loop syntax like: for(i <- 1 to 2) equal to for(i <- (1).to(2))
@@ -12,6 +12,59 @@ object Training extends App{
   //val arr = new Array[String](3)
   //arr(0) = "Zero" //equals to arr.update(0, "Zero")
   //print(arr(0)) //equals to print(arr.apply(0))
+
+  /*
+  val oneTwoThreeFour = oneTwo ::: threeFour
+  println(oneTwo + " and " + threeFour + " were not mutated.")
+  println("Thus, " + oneTwoThreeFour + " is a new list.")
+
+  val twoThree = List(2, 3)
+  val oneTwoThree = 1 :: twoThree
+  println(oneTwoThree)
+  println(twoThree)
+
+  ::: - add list to each other
+  :: - "cons" - add new element to the beggining of List (above example)
+
+  is actually a simple rule to
+  remember: If a method is used in operator notation, such as a * b, the method is invoked on the left
+  operand, as in a.*(b)—unless the method name ends in a colon. If the method name ends in a colon, the
+  method is invoked on the right operand.
+
+
+  Use the new keyword when you want to refer to a class's own constructor:
+  class Foo { }
+  val f = new Foo
+
+  Omit new if you are referring to the companion object's apply method:
+  class Foo { }
+  object Foo {
+    def apply() = new Foo
+  }
+
+  // Both of these are legal
+  val f = Foo()
+  val f2 = new Foo
+
+  ***PL w skrocie, new uzywamy gdy chcemy odwolac sie do konstruktora danej klasy, natomiast bez new
+        musimy stworzyc object w danej klasie (chyba taka sama nazwa) a w nim metode apply, ktora zachowauje sie
+        jak konstruktor, wtedy przy tworzeniu objektu wlasnie ta funkcja apply bedzie wywolana
+
+  PAGE 48 - usefull LIST's methods
+
+  Tuples:
+  val pair = (99, "Luftballons")
+  println(pair._1)
+  println(pair._2)
+
+  //todo sets?????? maps ????? hashset?
+  var jetSet = Set("Boeing", "Airbus")
+  jetSet += "Lear"
+  println(jetSet.contains("Cessna"))
+
+
+   */
+
 
 
 }
